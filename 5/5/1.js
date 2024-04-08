512594a47856514da6b6d2338b5d26fc43e20e01
kolinrok1 <109362691+kolinrok1@users.noreply.github.com>
2024-04-08 19:36:47 +0300

    5dz

---
+++ 5/5/1.js
@@ -0,0 +1,19 @@
+/*
+Дан объект numbers. 
+Необходимо в консоль вывести все значения объекта, которые больше или равны 3.
+*/
+
+const numbers = {
+  key1: 12,
+  key2: 2,
+  key3: 4,
+  key4: 1,
+  key5: -244,
+  key6: 0,
+  key7: 7,
+};
+for (const values in numbers) {
+  if(numbers[values] >= 3){
+    console.log(numbers[values])
+  }
+}
