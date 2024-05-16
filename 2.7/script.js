4542e3ce72bce4702ac64ec6efcb53cc3a977334
kolinrok1 <109362691+kolinrok1@users.noreply.github.com>
2024-05-16 18:43:01 +0300

    2.77

--- 2.7/script.js
+++ 2.7/script.js
@@ -30,10 +30,21 @@ rangeEL.addEventListener('change', function (e) {
     videoEl.currentTime =  e.target.value / 100 * videoEl.duration;
 });
 videoEl.addEventListener('timeupdate', function (e) {
-    rangeEL.setAttribute("value",Math.round (videoEl.currentTime / videoEl.duration * 100))
+    rangeEL.setAttribute("value", Math.round (videoEl.currentTime / videoEl.duration * 100))
 });
 
-
+const valueEL = document.createElement('input');
+valueEL.setAttribute("type", "range");
+valueEL.setAttribute("min", "0");
+valueEL.setAttribute("max", "100");
+valueEL.setAttribute("value", "0");
+videoEl.addEventListener('loadeddata', function (e) {
+    valueEL.setAttribute("value", videoEl.volume * 100)
+});
+valueEL.addEventListener('change', function (e) {
+    videoEl.volume = e.target.value / 100;
+});
+divEl.appendChild(valueEL);
 divEl.appendChild(rangeEL);
 divEl.appendChild(pauseEll);
 divEl.appendChild(playEll);
