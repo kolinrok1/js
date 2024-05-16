77dee1f78fe0d1fcdf47227379ec4cda12df7fca
kolinrok1 <109362691+kolinrok1@users.noreply.github.com>
2024-05-16 18:28:43 +0300

    2.7

---
+++ 2.7/script.js
@@ -0,0 +1,39 @@
+const videoEl = document.createElement('video');
+videoEl.setAttribute('src', "video/sample-5s.mp4");
+videoEl.classList.add('video');
+const playEll = document.createElement('div');
+playEll.classList.add('play');
+playEll.innerHTML="play";
+
+const pauseEll = document.createElement('div');
+pauseEll.classList.add('pause');
+pauseEll.innerHTML="pause";
+
+const divEl = document.querySelector('.conteiner');
+divEl.appendChild(videoEl);
+
+
+playEll.addEventListener('click', function (e) {
+    videoEl.play();
+    console.log(videoEl.duration, videoEl.currentTime);
+});
+pauseEll.addEventListener('click', function (e) {
+    videoEl.pause();
+});
+
+const rangeEL = document.createElement('input');
+rangeEL.setAttribute("type", "range");
+rangeEL.setAttribute("min", "0");
+rangeEL.setAttribute("max", "100");
+rangeEL.setAttribute("value", "0");
+rangeEL.addEventListener('change', function (e) {
+    videoEl.currentTime =  e.target.value / 100 * videoEl.duration;
+});
+videoEl.addEventListener('timeupdate', function (e) {
+    rangeEL.setAttribute("value",Math.round (videoEl.currentTime / videoEl.duration * 100))
+});
+
+
+divEl.appendChild(rangeEL);
+divEl.appendChild(pauseEll);
+divEl.appendChild(playEll);
